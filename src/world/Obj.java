d55070ae669e7e845e0a26c7491cff4700312237
418cat <benjaminbadina@gmail.com>
2023-07-11 23:01:23 +0200

    restarting

--- src/world/Obj.java
+++
@@ -1,5 +0,0 @@
-package world;
-
-public abstract class Obj {
-	int[] coords;
-}
--- src/world/objects/Shapes/Cube.java
+++
@@ -1,7 +0,0 @@
-package world.objects.Shapes;
-
-import world.objects.Shape;
-
-public class Cube extends Shape {
-	
-}
